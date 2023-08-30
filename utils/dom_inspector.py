3e5767bde30355b8ce0a017bdcefb62e7071858e
WongJingGitt <WongJingGit@163.com>
2023-08-30 12:18:30 +0800

    增加注释，更改识别数据返回方式

--- utils/dom_inspector.py
+++ utils/dom_inspector.py
@@ -9,11 +9,49 @@ import cv2
 
 
 class DOMInspector:
-    def __init__(self, yolo_model: str):
-        self._yolo_model = yolo_model
-        self._result = []
+    """
+    DOMInspector类用于在图像中检测DOM元素，并执行OCR识别。
 
-    def __call__(self, image: bytes, lang: str = 'ch', dom_search: typing.Callable = None, **kwargs):
+    Args:
+        yolo_model (str): YOLO模型的路径。
+
+    Attributes:
+        _yolo_model (str): YOLO模型的路径。
+    """
+    def __init__(self, yolo_model: str):
+        """
+        初始化DOMInspector类。
+
+        Args:
+            yolo_model (str): YOLO模型的路径。
+        """
+        self._yolo_model = yolo_model
+
+    def __call__(self, image: bytes, lang: str = 'ch', dom_search: typing.Callable = None, **kwargs) -> list[dict]:
+        """
+        在图像中检测DOM元素并执行OCR识别。
+
+        Args:
+            image (bytes): 输入的图像字节数据。
+            lang (str, optional): OCR识别使用的语言。默认为'ch'。
+            dom_search (Callable, optional): 用于筛选DOM元素的自定义搜索函数。默认为None。
+
+        Returns:
+            list[dict]: 包含DOM元素信息的列表。
+
+        Notes:
+            - 输入图像应为字节数据。
+            - 如果提供了dom_search函数，则只返回满足搜索条件的DOM元素。
+
+        Example:
+                browser_launcher = BrowserLauncher(headless=True)
+                browser = browser_launcher.browser
+                page = browser_launcher.page
+                page.goto('https://www.bilibili.com/', timeout=0)
+                screenshot = page.screenshot()
+                dom_inspector = DOMInspector(yolo_model=path.join(ProjectPath.root_path, 'bilibili_best.pt'))
+                result = dom_inspector(image=screenshot, dom_search=lambda item: item.get('name') == 'channel-link' and '鬼畜' in item.get('text'))
+        """
         image_array = np.frombuffer(image, dtype=np.uint8)
         image_cv = cv2.imdecode(image_array, flags=cv2.IMREAD_COLOR)
 
@@ -25,6 +63,8 @@ class DOMInspector:
         image_object.close()
 
         ocr = PaddleOCR(use_angle_cls=True, lang=lang)
+
+        dom_list = []
         for index, item in enumerate(result):
             item = json.loads(item.tojson())
             if len(item) <= 0:
@@ -43,10 +83,7 @@ class DOMInspector:
                 result_with_text = {"box": box, "name": name, "class": _class, "confidence": confidence,
                                     "text": text_result}
                 if isinstance(dom_search, typing.Callable) and dom_search(result_with_text):
-                    self._result.append(result_with_text)
+                    dom_list.append(result_with_text)
                 elif not dom_search:
-                    self._result.append(result_with_text)
-
-    @property
-    def get(self) -> list[dict[str or list or dict]]:
-        return self._result
+                    dom_list.append(result_with_text)
+        return dom_list
